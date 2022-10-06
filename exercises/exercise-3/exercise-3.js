640df1e6fc7db3635bb7f31dffe427803b735555
chandredewet <chandredewet@gmail.com>
2022-10-07 00:34:21 +0200

    completed exercise 3

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,18 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+function receiptPrint(order) {
+  let total = 0;
+  console.log("QTY\tITEM\t\t\tTOTAL");
+  order.forEach(el => {
+    let { quantity, itemName, unitPrice} = el;
+
+    console.log(`${quantity}\t${itemName}\t\t${unitPrice.toFixed(2)}`)
+    total += (unitPrice * quantity)
+  })
+  console.log("Total:", total);
+}
+
+
+receiptPrint(order);
